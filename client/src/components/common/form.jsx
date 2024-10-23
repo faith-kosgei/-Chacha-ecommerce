56cd8d254ff23bc9073f4439567cef4078dade57
faith-kosgei <kosgeif7@gmail.com>
2024-10-23 12:58:49 +0300

    creating forms:smile

--- client/src/components/common/form.jsx
+++ client/src/components/common/form.jsx
@@ -1,10 +1,17 @@
-
-
-
-function CommonForm() {
-    return ( 
-        <form action="" className=""></form>
-     );
+function CommonForm({ formControls }) {
+  return (
+    <form action="" className="">
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}></div>
+        ))}
+        <label className="mb-1">{controlItem.label}</label>
+        {
+            renderInputByComponentType(controlItem)
+        }
+      </div>
+    </form>
+  );
 }
 
 export default CommonForm;
